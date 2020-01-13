58b8e68706b594a25840972a015a5bca8564c60b
tanvi177 <andrdruhy@gmail.com>
2020-01-13 23:08:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taxassistpro/Income3.java
@@ -0,0 +1,14 @@
+package com.example.taxassistpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Income3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_income3 );
+    }
+}
---
+++ app/src/main/java/com/example/taxassistpro/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.example.taxassistpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_otp );
+    }
+}
